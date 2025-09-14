8bd6e7e488def280cdd7476ce0e62de31da22fce
Kentino4452 <kentagiacomo.frei@uzh.ch>
2025-09-14 23:44:10 +0200

    Update SceneDelegate.swift

--- SceneDelegate.swift
+++ SceneDelegate.swift
@@ -16,19 +16,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         UserDefaults.standard.set(testToken, forKey: "authToken")
         print("🔑 Token di test salvato in UserDefaults")
 
-        let hasSeenOnboarding = UserDefaults.standard.bool(forKey: "hasSeenOnboarding")
-
-        if !hasSeenOnboarding {
-            window?.rootViewController = OnboardingViewController()
-            window?.makeKeyAndVisible()
-            return
-        }
-
         // 🔴 Saltiamo login/registrazione → entriamo direttamente nell'app
         window?.rootViewController = MainTabBarController()
         window?.makeKeyAndVisible()
     }
 }
+
 
 
 
